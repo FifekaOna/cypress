1316c005746e93465b430468980dd1ec20425cab
ona <fifekawahid@gmail.com>
2020-06-15 20:54:48 +0700

    Data Driven from JSON

--- cypress/integration/fabelio/TC_Search.spec.js
+++ cypress/integration/fabelio/TC_Search.spec.js
@@ -1,22 +1,51 @@
 /// <reference types="cypress" />
 
 
+
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false
   })
   
 
 describe('search',function(){
+
+    before(function(){
+        cy.fixture('items').then(function(data){
+            this.data=data
+        })
+    })
+    
     it('search_product',function(){
-        cy.visit('https://qa.fabelio.com')
-        cy.get('input[placeholder="Cari produk"]').type('cessi')
-        cy.get('input[placeholder="Cari produk"]').type('{enter}')
-        cy.contains('Dining Chair').parent().within(()=>{
-            cy.get('a').click({ force: true })
+
+        var i=0
+        while (this.data[i] != null){
+            cy.visit('https://qa.fabelio.com')
+            cy.get('input[placeholder="Cari produk"]').type(this.data[i].search)
+            cy.get('input[placeholder="Cari produk"]').type('{enter}')
+
+            if (this.data[i].lookFor != null) {
+                cy.contains(this.data[i].lookFor).parent().within(()=>{
+                    cy.get('a').click({ force: true })
+                }
+                )
+            }
+            
+           
+            cy.get('button[id="buyNow"]').click({ force: true })
+            
+
+            // cy.wait(50000)
+            cy.server()
+            cy.route('POST','/rest/default/V1/checkout-step').as('checkout')
+            cy.wait('@checkout', { timeout: 60000 })
+
+            cy.screenshot();
+
+            i++
+           
         }
-        )
-        const x= cy.get('button[id="buyNow"]').click({ force: true })
-        cy.screenshot();
+        
     })
 })
 
