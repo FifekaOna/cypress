e190db119fe5613d794aa67f0dd42be839a51fa2
ona <fifekawahid@gmail.com>
2020-06-16 12:49:09 +0700

    integrate test case with JSON file from CSV

--- cypress/integration/fabelio/TC_Search.spec.js
+++ cypress/integration/fabelio/TC_Search.spec.js
@@ -11,35 +11,39 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('search',function(){
 
     before(function(){
-        cy.fixture('items').then(function(data){
+
+        // Get data from JSON file 
+        cy.fixture('testDataFromCSV').then(function(data){
             this.data=data
         })
     })
     
     it('search_product',function(){
-
+        // looping every data 
         var i=0
         while (this.data[i] != null){
+            //visit page
             cy.visit('https://qa.fabelio.com')
+            // search in search box
             cy.get('input[placeholder="Cari produk"]').type(this.data[i].search)
             cy.get('input[placeholder="Cari produk"]').type('{enter}')
 
+            // click desired item based on criteria
             if (this.data[i].lookFor != null) {
                 cy.contains(this.data[i].lookFor).parent().within(()=>{
                     cy.get('a').click({ force: true })
                 }
                 )
             }
-            
-           
+            // click buy 
             cy.get('button[id="buyNow"]').click({ force: true })
             
-
-            // cy.wait(50000)
+            // wait for all XHR finished and check-out page appeared
             cy.server()
             cy.route('POST','/rest/default/V1/checkout-step').as('checkout')
             cy.wait('@checkout', { timeout: 60000 })
 
+            // take screenshot
             cy.screenshot();
 
             i++
